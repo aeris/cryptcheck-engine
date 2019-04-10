2451e7ac8569cd1c8062a2502ef6a9949da16499
aeris <aeris@imirhil.fr>
2019-04-10 14:37:17 +0200

    String#from/to_hex from spec to lib

--- lib/cryptcheck/engine/fixtures/string.rb
+++ lib/cryptcheck/engine/fixtures/string.rb
@@ -1,4 +1,12 @@
 class String
+	def from_hex
+		self.b.gsub(/\s/, '').scan(/../).collect { |h| h.hex.chr }.join.b
+	end
+
+	def to_hex
+		self.b.each_byte.map { |b| "%02x" % b.ord }.join.upcase
+	end
+
 	# From https://api.rubyonrails.org/classes/ActiveSupport/Inflector.html#method-i-constantize
 	def constantize
 		names = self.split '::'
--- spec/fixtures/string.rb
+++
@@ -1,9 +0,0 @@
-class String
-	def from_hex
-		self.b.gsub(/\s/, '').scan(/../).collect { |h| h.hex.chr }.join.b
-	end
-
-	def to_hex
-		self.b.each_byte.map { |b| "%02x" % b.ord }.join.upcase
-	end
-end
